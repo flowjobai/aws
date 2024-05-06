d32f1fdda227e1db4a8b73f5a2290407c8c0b643
steve-scorn <sskillcorn@gmail.com>
2024-05-06 22:28:37 +0100

    Add region to config

--- auth/amplify-client.ts
+++ auth/amplify-client.ts
@@ -4,10 +4,12 @@ const config = {
     Auth: {
         Cognito: {
             userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
-            userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID
+            userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID,
+            region: process.env.NEXT_PUBLIC_COGNITO_REGION || process.env.NEXT_PUBLIC_REGION
         },
     },
 };
 
+// @ts-ignore
 Amplify.configure(config, { ssr: true });
 console.log("Cognito configured", config.Auth.Cognito.userPoolId);
