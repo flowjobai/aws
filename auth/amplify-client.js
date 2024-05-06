7f2860e53a1fb961bdd1fbc2cc89cadfe8f33010
steve-scorn <sskillcorn@gmail.com>
2024-05-06 19:39:25 +0100

    Make amplify client js

--- auth/amplify-client.js
+++ auth/amplify-client.js
@@ -3,9 +3,8 @@ import { Amplify } from "aws-amplify";
 const config = {
     Auth: {
         Cognito: {
-            userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
-            userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID!,
-            region: process.env.NEXT_PUBLIC_COGNITO_REGION!,
+            userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
+            userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_CLIENT_ID
         },
     },
 };
