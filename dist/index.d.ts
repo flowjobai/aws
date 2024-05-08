3195c9da6611dc7ec197d08a5f623439d0bfd508
steve-scorn <sskillcorn@gmail.com>
2024-05-08 07:32:31 +0100

    Export batch

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-export { execute, formatted } from "./db/rds-data-api";
+export { execute, formatted, batch } from "./db/rds-data-api";
 export { getCurrentUserServer } from "./auth/amplify-server";
 import Login from "./auth/Login";
 export { Login };
